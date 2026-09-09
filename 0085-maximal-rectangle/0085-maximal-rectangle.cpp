02e90dc630062499b65578b84ba4763405d6e3ee
pawan-1089 <pawan9580099635@gmail.com>
2026-09-10 00:53:30 +0530

    Time: 7 ms (65.82%), Space: 18.8 MB (89.25%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        long long area=0;
-        long long ans=0;
-        int n=matrix.size();
-        int m=matrix[0].size();
-        vector<vector<int>>presum(n,vector<int>(m,0));
-        for(int i=0; i<m; i++){
-            int sum=0;
-            for(int j=0; j<n; j++){
-                sum+=matrix[j][i]-'0';
-                if(matrix[j][i]=='0'){
-                    sum=0;
+        long long area = 0;
+        long long ans = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>height(m,0);
+        for (int i = 0; i<n; i++) {
+            for (int j = 0; j <m; j++) {
+                if(matrix[i][j]=='1'){
+                    height[j]++;
                 }
-                presum[j][i]=sum;
+                else height[j]=0;
             }
-        }
-        for(int i=0; i<n; i++){
-            stack<int>st;
-            //computer each row like histogram
-            for(int j=0; j<m; j++){
-                while(!st.empty() && presum[i][st.top()]>presum[i][j]){
-                    int el=st.top();
+            stack<int> st;
+            // computer each row like histogram
+            for (int j = 0; j < m; j++) {
+                while (!st.empty() && height[st.top()] > height[j]) {
+                    int el = st.top();
                     st.pop();
-                    int nse=j;
-                    int pse=st.empty()?-1:st.top();
-                    area=1LL*presum[i][el]*(nse-pse-1);
-                    ans=max(ans,area);
+                    int nse = j;
+                    int pse = st.empty() ? -1 : st.top();
+                    area = 1LL * height[el]*(nse - pse - 1);
+                    ans = max(ans, area);
                 }
                 st.push(j);
             }
-            while(!st.empty()){
-                int el=st.top();
+            while (!st.empty()) {
+                int el = st.top();
                 st.pop();
-                int nse=m;
-                int pse=st.empty()?-1:st.top();
-                area=1LL*presum[i][el]*(nse-pse-1);
-                ans=max(ans,area);
+                int nse = m;
+                int pse = st.empty() ? -1 : st.top();
+                area = 1LL * height[el]*(nse - pse - 1);
+                ans = max(ans, area);
             }
         }
         return ans;
