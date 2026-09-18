889b31e9b7642ec7060047966aaa8a38edc71569
pawan-1089 <pawan9580099635@gmail.com>
2026-09-18 09:35:57 +0530

    Time: 0 ms (100%), Space: 69.5 MB (96.64%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -11,13 +11,13 @@ public:
             if(nums[r]==0){
                 zeros++;
             }
-            while(zeros>k && l<n){
+            if(zeros>k){
                 if(nums[l]==0){
                     zeros--;
                 }
                 l=l+1;
             }
-            if(zeros<=k){
+            if(zeros<=k ){
                 maxlen=max(maxlen,r-l+1);
             }
             r++;
