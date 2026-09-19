5526cf582271f10d456b7d5d1994af6702fefcfe
pawan-1089 <pawan9580099635@gmail.com>
2026-09-19 14:40:39 +0530

    Time: 52 ms (22.79%), Space: 84.8 MB (83.22%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,14 +8,16 @@ public:
         unordered_map<int,int>mpp;
         while(r<n){
             mpp[fruits[r]]++;
-            while(mpp.size()>2){
+            if(mpp.size()>2){
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]]==0){
                     mpp.erase(fruits[l]);
                 }
                 l++;
             }
-            maxlen=max(maxlen,r-l+1);
+            if(mpp.size()<=2){
+                maxlen=max(maxlen,r-l+1);
+            }
             r++;
         }
         return maxlen;
