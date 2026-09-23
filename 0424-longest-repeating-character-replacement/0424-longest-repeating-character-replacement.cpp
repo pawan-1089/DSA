3481855226675dd35679956a5ad0e458f2e3ba75
pawan-1089 <pawan9580099635@gmail.com>
2026-09-23 18:20:20 +0530

    Time: 0 ms (100%), Space: 10.8 MB (71.07%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -10,7 +10,7 @@ public:
         while(r<s.length()){
             hash[s[r]-'A']++;
             maxf=max(maxf,hash[s[r]-'A']);
-            while((r-l+1)-maxf>k){
+            if((r-l+1)-maxf>k){
                 hash[s[l]-'A']--;
                 maxf=0;
                 for(int i=0; i<26; i++){
